b952099c725ff884b611a3e2af65991c8c2d227a
Elizabeth <lizbeepmc@gmail.com>
2023-11-22 22:04:16 +0300

    Main

---
+++ react-my-portfolio/src/components/rightmenu.jsx
@@ -0,0 +1,11 @@
+import "./rightmenu.css";
+function Rightmenu() {
+  return (
+    <>
+      <aside>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quam ducimus in, iusto ad officia at incidunt doloribus libero, et animi neque dolore a delectus illum corrupti asperiores quidem molestias.</p>
+      </aside>
+    </>
+  );
+}
+export default Rightmenu;
