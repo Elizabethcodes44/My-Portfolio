0295379dcc17539de052020ac19d28ccb05a6ca9
Elizabeth <lizbeepmc@gmail.com>
2023-11-24 17:34:55 +0300

    deploying

--- react-my-portfolio/src/components/footer.jsx
+++ react-my-portfolio/src/components/footer.jsx
@@ -7,11 +7,12 @@ function Footer() {
       <footer>
         <div>
           <a href="https://github.com/Elizabethcodes44">
-            <img src={gitLogo} className="logo" alt="Vite logo" />
+            <img src={gitLogo} className="logo" alt="gitlogo" target="_blank" />
           </a>
           <a href="https://www.instagram.com/bukonla_s/x">
-            <img src={instagramLogo} className="logo react" alt="React logo" />
+            <img src={instagramLogo} className="logo" alt="React logo"  target="_blank"/>
           </a>
+
         </div>
       </footer>
     </>
