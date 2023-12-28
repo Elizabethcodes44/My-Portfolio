637abc3dfdef5d53ca0b3e817a25a639e30e5b4a
Elizabeth <lizbeepmc@gmail.com>
2023-12-28 21:19:25 +0300

    Header completed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,26 @@
 import "./header.css";
 import { useNavigate } from "react-router-dom";
 function Header() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <>
       <header>
-        <p onClick={()=>navigate('/')}>Home</p>
+        <nav>
+          <h2 className="initials">EK</h2>
+          <ul className="list">
+            <li>
+              <p className="par" onClick={() => navigate("/")}>
+                HOME
+              </p>
+            </li>
+            <li>
+              <p className="par">PROJECTS</p>
+            </li>
+            <li>
+              <p className="par">CONTACT</p>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   );
