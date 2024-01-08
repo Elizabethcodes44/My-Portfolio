f6e8073152606a131af3d3fd3c8051ccdb6d1ef6
Elizabeth <lizbeepmc@gmail.com>
2024-01-08 23:47:40 +0300

    netlify build

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 import gitLogo from "../assets/github1.svg";
 import instagramLogo from "../assets/instagram1.svg";
-import linkedinLogo from "../../src/assets/linkedin.svg";
+
 import twitterLogo from "../../src/assets/twitterx.svg";
 
 import "./footer.css";
@@ -27,14 +27,7 @@ function Footer() {
                 target="_blank"
               />
             </a>
-            <a href="https://www.linkedin.com/in/elizabeth-kujore-a8b116128/">
-              <img
-                src={linkedinLogo}
-                className="logo"
-                alt="linkedinlogo"
-                target="_blank"
-              />
-            </a>
+           
             <a href="https://twitter.com/SonugaZ">
               <img
                 src={twitterLogo}
