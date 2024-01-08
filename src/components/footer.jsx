ed57c2679e0b02d9bb256094797b488dee26faae
Elizabeth <lizbeepmc@gmail.com>
2024-01-08 22:00:13 +0300

    header, footer and home completed

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
-import gitLogo from "../assets/Github.svg";
-import instagramLogo from "../assets/Instagram.svg";
-import linkedinLogo from "../assets/Linkedin.svg";
-import twitterLogo from "../assets/Twitter.svg";
+import gitLogo from "../assets/github1.svg";
+import instagramLogo from "../assets/instagram1.svg";
+import linkedinLogo from "../assets/linkedin.svg";
+import twitterLogo from "../assets/twitterx.svg";
 
 import "./footer.css";
 function Footer() {
